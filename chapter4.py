0db0c71652f41aff5701a6eee9e8821e750f76a5
Jacob Taylor Cassady <jacobtaylorcassady@outlook.com>
2023-04-30 19:29:07 -0400

    Finished day 4 notes

--- chapter4.py
+++ chapter4.py
@@ -10,7 +10,28 @@ for magician in magicians:
         print(character)    
 print("end")
 
-# Using the range() function
+# Using the range(START, STOP, INCREMENT=1) function
 print("\nUsing the range() function example")
-for samuels_value in range(1, 5):
+for samuels_value in range(1, 10):
     print(samuels_value)
+
+print("")
+
+# range() example 2
+squares = []                # Initialize an empty list
+for value in range(1, 11):  # Loop over the range 1-10 by 1
+    square = value**2       # Square each value and store in 'square'
+    squares.append(square)  # store 'square' in list 'squares'
+print(squares)              # print the final list
+
+print("")
+
+# Simple statistics with a list of numbers
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print("Digits: " + str(digits))
+minimum_value = min(digits)
+print("minimum value: " + str(minimum_value))
+maximium_value = max(digits)
+print("maximium value: " + str(maximium_value))
+total = sum(digits)
+print("total: " + str(total))
