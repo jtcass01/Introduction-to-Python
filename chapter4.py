d334bd2fe1a07a6135c930b7a9fa414dc428bd23
Jacob Taylor Cassady <jacobtaylorcassady@outlook.com>
2023-04-30 19:47:44 -0400

    Finished chapter 4 lecture

--- chapter4.py
+++ chapter4.py
@@ -24,6 +24,21 @@ for value in range(1, 11):  # Loop over the range 1-10 by 1
     squares.append(square)  # store 'square' in list 'squares'
 print(squares)              # print the final list
 
+# Homework!!! Update this code to print
+# [100, 81, 64, 49, 36, 25, 16, 9, 4, 1]
+# SOLUTION 1
+squares = []                    # Initialize an empty list
+for value in range(10, 0, -1):  # Loop over the range 10-1 by -1
+    square = value**2           # Square each value and store in 'square'
+    squares.append(square)      # store 'square' in list 'squares'
+print(squares)                  # print the final list
+# SOLUTION 2
+squares = []                          # Initialize an empty list
+for value in reversed(range(1, 11)):  # Loop over the range 1-10 by 1
+    square = value**2                 # Square each value and store in 'square'
+    squares.append(square)            # store 'square' in list 'squares'
+print(squares)                        # print the final list
+
 print("")
 
 # Simple statistics with a list of numbers
@@ -35,3 +50,19 @@ maximium_value = max(digits)
 print("maximium value: " + str(maximium_value))
 total = sum(digits)
 print("total: " + str(total))
+
+# Tuples
+## Original dimensions
+dimensions = (200, 50)
+print("Original dimensions:", dimensions)
+for dimension in dimensions:
+    print(dimension)
+
+## Modified dimensions
+dimensions = (400, 100)
+print("Modified dimensions:", dimensions)
+for dimension in dimensions:
+    print(dimension)
+
+# EXPECT ERROR!!!!
+dimensions[0] = 10
