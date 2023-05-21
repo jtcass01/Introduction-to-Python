1bcbe7b414470464a177c8894eacb40af83c4c4b
Jacob Taylor Cassady <jacobtaylorcassady@outlook.com>
2023-05-21 19:06:25 -0400

    Deleted old files. added to chapter 7 notes

--- Chapter7_1.py
+++ Chapter7_1.py
@@ -1,16 +1,24 @@
-message=input("Tell me your name: ")
+#message=input("Tell me your name: ")
+message = "Kyongchan"
 print("Hello, " + message + "!")
 
-age = input("How old are you? ")
+#age = input("How old are you? ")
+age = 15
 print("age", age)
 age = int(age)     # Cast age to an integer
 new_age = age - 1
 print("new_age", new_age)
 
 
-height = input("How tall are you, in inches?")
+height = input("How tall are you, in inches? ")
+weight = input("How much do you weight, in lbs.? ")
 height = int(height)
-if height >= 36:
-    print("\nYou're tall enough to ride!")
+weight = int(weight)
+if height >= 36 and weight <= 200:
+    print("\nYou're tall and light enough to ride!")
+elif height >= 36 and weight > 200:
+    print("\nSorry, you are tall enough but you are too heavy.")
+elif height > 112:
+    print("\nHow's the weather up there? You can't ride!")
 else:
-    print("\nYou'll be able to ride when you're a little older.")
+    print("\nMaybe next time!")
