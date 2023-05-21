b6cee819d60cdcf5662ba0321a5dd132b3db637c
Jacob Taylor Cassady <jacobtaylorcassady@outlook.com>
2023-05-21 19:35:41 -0400

    Finished Chapter 7 part 2.

--- chapter7_2.py
+++ chapter7_2.py
@@ -3,3 +3,35 @@ current_number = 1
 while current_number <= 5:
     print(current_number)
     current_number += 1         # current_number = current_number + 1
+
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program."
+active = True
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
+
+
+# Confirmed users example
+## Start with users that need to be verified,
+## and an empty list to hold confirmed users.
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+
+# Verify each user until there are no more unconfirmed users.
+# Move each verified user into the list of confirmed users.
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+
+    print("Verifying user: " + current_user.title())
+    confirmed_users.append(current_user)
+
+# Display all confirmed users
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
