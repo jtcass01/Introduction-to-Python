fd315a2e61717d199464451cc39ca3380cff7653
Jacob Taylor Cassady <jacobtaylorcassady@outlook.com>
2023-05-28 18:17:14 -0400

    Moved files inside of Chapter directories.

--- Chapter 7/chapter7_2.py
+++ Chapter 7/chapter7_2.py
@@ -26,10 +26,10 @@ confirmed_users = []
 # Verify each user until there are no more unconfirmed users.
 # Move each verified user into the list of confirmed users.
 while unconfirmed_users:
-    current_user = unconfirmed_users.pop()
+    current_user = unconfirmed_users.pop()           # Remove an unconfirmed user from the list.
 
     print("Verifying user: " + current_user.title())
-    confirmed_users.append(current_user)
+    confirmed_users.append(current_user)             # Add the removed user to the new list.
 
 # Display all confirmed users
 print("\nThe following users have been confirmed:")
