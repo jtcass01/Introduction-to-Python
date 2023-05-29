38b084caf12a37112661e73126693cbc546c54c1
Jacob Taylor Cassady <jacobtaylorcassady@outlook.com>
2023-05-28 20:00:02 -0400

    Finished class 5/28/23

--- Chapter 8 - Function/making_order.py
+++ Chapter 8 - Function/making_order.py
@@ -2,4 +2,4 @@
 import order
 
 order.make_pizza(12, 'ham', 'pineapple', 'extra cheese', 'meatballs', 'anchovies')
-order.make_sandwich(12, 'chicken', 'bacon', 'extra ranch')
+order.make_sandwich(6, 'chicken', 'bacon', 'extra ranch')
