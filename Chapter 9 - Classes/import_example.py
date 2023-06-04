33a0fec3745a1a4556a96aa4df68f07c1d8ca938
Jacob Taylor Cassady <jacobtaylorcassady@outlook.com>
2023-06-04 19:54:46 -0400

    Updated import example

--- Chapter 9 - Classes/import_example.py
+++ Chapter 9 - Classes/import_example.py
@@ -1,9 +1,9 @@
 # Import example
-from car import Car
+import car as c
 from dog import Dog, describe_dog
 
 # Create an old car with a different default odometer reading
-jacobs_car = Car('toyota', 'celica', 2000, 3000, initial_reading=230000)
+jacobs_car = c.Car('toyota', 'celica', 2000, 3000, initial_reading=230000)
 print(jacobs_car.get_descriptive_name())
 
 my_dog = Dog('Michio', 1)   # Construct a dog object.
